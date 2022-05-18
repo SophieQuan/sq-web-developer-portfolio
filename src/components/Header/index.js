b3a18e3a3ce9a8c9b49ed1493c511bb2690060fd
SophieQuan <sophiequan7@gmail.com>
2022-05-17 23:50:26 -0400

    add favicon

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,10 +7,7 @@ function Header() {
   return (
     <header>
       <h2 className="col-lg-4 col-12">
-        <a
-          href="http://sophiequan.github.io/sq-web-developer-portfolio/about"
-          id="logo"
-        >
+        <a href="/" id="logo">
           Sophie<span>Quan</span>
         </a>
       </h2>
